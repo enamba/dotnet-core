c18af8b76a7332ea1b6d7a9c6c91dec299d9ce81
Eduardo Namba <eduardo@eduardo.cc>
2017-04-27 17:38:33 -0300

    fix to correct branch

--- Dockerfiles/script.sh
+++ Dockerfiles/script.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 mkdir -p /opt/dotnet-core/
-git clone -b v1 https://github.com/enamba/dotnet-core.git /opt/dotnet-core/
+git clone -b v0 https://github.com/enamba/dotnet-core.git /opt/dotnet-core/
 cd /opt/dotnet-core/
-git checkout v1
+git checkout v0
 dotnet restore
 dotnet run
