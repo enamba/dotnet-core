2b188d05a9d08889361ccb1f18de4daf39013fa2
Eduardo Namba <eduardo@eduardo.cc>
2017-05-10 13:37:38 -0300

    update to run pricing api from V2O

--- Dockerfiles/script.sh
+++ Dockerfiles/script.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 mkdir -p /opt/dotnet-core/
-git clone -b v0 https://github.com/enamba/dotnet-core.git /opt/dotnet-core/
+git clone -b master https://github.com/enamba/priceAPI-V2O.git  /opt/dotnet-core/
 cd /opt/dotnet-core/
-git checkout v0
-dotnet restore
-dotnet run
+git checkout pricingApiNova
+#dotnet restore
+#dotnet run
