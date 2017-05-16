2d157133ee4321dea0a80f52967ac8d122dff475
Eduardo Namba <eduardo@eduardo.cc>
2017-05-16 10:47:57 -0300

    trying some configurations...

--- Dockerfiles/api-pricing.sh
+++ Dockerfiles/api-pricing.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 # to run application in background
-su - application -c "bash -c 'cd /opt/priceAPI-V2O/; /usr/bin/dotnet Ecommerce.Preco.Hosts.Api.dll'" &
+#su - application -c "bash -c 'cd /opt/priceAPI-V2O/; /usr/bin/dotnet Ecommerce.Preco.Hosts.Api.dll'" &
+echo "run script"
 
 
