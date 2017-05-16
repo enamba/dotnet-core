6c09078f4d8aace78e89657bb2a228ef43fc3ee5
Eduardo Namba <eduardo@eduardo.cc>
2017-05-16 10:40:52 -0300

    FIX to run application in background

--- Dockerfiles/api-pricing.sh
+++ Dockerfiles/api-pricing.sh
@@ -1,2 +1,6 @@
 #! /bin/bash
-/bin/sh /etc/init.d/kestrel-apipreco.sh &
+
+# to run application in background
+su - application -c "bash -c 'cd /opt/priceAPI-V2O/; /usr/bin/dotnet Ecommerce.Preco.Hosts.Api.dll'" &
+
+
